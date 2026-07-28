8d628acef7034edf2d2b6f2b7731112a3695e99d
Guillaume Rovai <guillaumerovai@gmail.com>
2026-07-28 11:47:47 +0200

    refactor: remove unreferenced analytics panel and map widgets
    The analytics folder was reachable only through a barrel nothing imported.

--- client/src/components/analytics/index.ts
+++
@@ -1,7 +0,0 @@
-export { ForecastAnalyticsPanel } from './ForecastAnalyticsPanel';
-export { ForecastTypeSelector, FORECAST_TYPE_OPTIONS } from './ForecastTypeSelector';
-export { ProviderHorizonSelector } from './ProviderHorizonSelector';
-export { MetricCard, MetricCardSkeleton } from './MetricCard';
-export { ComparisonTable, ComparisonTableSkeleton } from './ComparisonTable';
-export { AnalyticsTimeSelector, TIME_RANGE_OPTIONS } from './AnalyticsTimeSelector';
-export { AccuracyTrendChart, AccuracyTrendChartSkeleton } from './AccuracyTrendChart';
